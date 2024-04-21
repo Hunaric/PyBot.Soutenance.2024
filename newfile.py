e09ba8c5c798ac41339e410a849800bd53fff42a
NATH FANG <nathanaelfangnon@gmail.com>
2024-04-21 20:40:21 +0100

    update of the virtualenv because of error by the last moving of the project directory

---
+++ newfile.py
@@ -0,0 +1,47 @@
+import PyPDF2
+import pyttsx3
+
+def lire_pdf(chemin_pdf, voix):
+    # Ouvrir le fichier PDF
+    with open(chemin_pdf, 'rb') as fichier:
+        lecteur_pdf = PyPDF2.PdfReader(fichier)
+        
+        # Lire chaque page du PDF
+        for num_page in range(len(lecteur_pdf.pages)):
+            page = lecteur_pdf.pages[num_page]
+            texte = page.extract_text()
+            
+            # Lire le texte avec la voix sélectionnée
+            lire_texte(texte, voix)
+
+def lire_texte(texte, voix):
+    # Initialiser le moteur de synthèse vocale
+    moteur = pyttsx3.init()
+    
+    # Sélectionner la voix
+    voix_feminine = None
+    voix_masculine = None
+    for voix_disponible in moteur.getProperty('voices'):
+        if "femme" in voix_disponible.name.lower():
+            voix_feminine = voix_disponible.id
+        elif "homme" in voix_disponible.name.lower():
+            voix_masculine = voix_disponible.id
+            
+    if voix == "féminine" and voix_feminine:
+        moteur.setProperty('voice', voix_feminine)
+    elif voix == "masculine" and voix_masculine:
+        moteur.setProperty('voice', voix_masculine)
+        
+    # Lire le texte
+    moteur.say(texte)
+    moteur.runAndWait()
+
+# Chemin vers le fichier PDF
+chemin_pdf = 'plan_esme.pdf'
+# chemin_pdf = 'C:/Users/LENOVO/Desktop/PyBot/plan_esme.pdf'
+
+# Demander à l'utilisateur de choisir la voix
+voix = input("Choisissez une voix (féminine/masculine) : ").lower()
+
+# Lire le PDF avec la voix sélectionnée
+lire_pdf(chemin_pdf, voix)
